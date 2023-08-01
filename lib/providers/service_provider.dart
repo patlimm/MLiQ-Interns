71b54f31baa1f629605458037cff1e65e0391029
FRANCIS_SANCHEZ <sanchez.francispup@gmail.com>
2023-08-01 10:56:34 +0800

    SW - Sanchez "Select a Practice Update"

--- lib/providers/service_provider.dart
+++ lib/providers/service_provider.dart
@@ -1,3 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final isDarkThemeProvider = StateProvider<bool>((ref) => false);
+
+final List<String> practiceNames = [
+  'Adjusting to Health',
+  'Advance Care Chiropractic',
+  'Alexandria Neck & Back Center',
+  'Barham Chiropractic',
+  'Bethesda Chiropractic Center a Pamercare Clinic',
+  'Better Life Chiropractic & Wellness',
+  'Blue Sky Family Wellness Chiropractic',
+  'Buffalo Chiropractic',
+  'Calliope Chiropractic',
+  'Chiro One Edwardsville',
+  'Chiro One St. Peters',
+  'Chiro One St. Peters',
+];
+
+class SelectedPracticeIndexNotifier extends StateNotifier<int> {
+  SelectedPracticeIndexNotifier() : super(-1);
+
+  void setSelectedIndex(int index) {
+    state = index;
+  }
+}
+
+final selectedPracticeIndexProvider =
+    StateNotifierProvider<SelectedPracticeIndexNotifier, int>((ref) {
+  return SelectedPracticeIndexNotifier();
+});
--- lib/routes/app_route_config.dart
+++ lib/routes/app_route_config.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mliq/pages/auth/login_page.dart';
 import 'package:mliq/pages/parent_page.dart';
+import 'package:mliq/pages/select_practice.dart';
 import 'package:mliq/pages/splash_screen/splash_screen.dart';
 import 'package:mliq/routes/account_tab/account_tab_route.dart';
 import 'package:mliq/routes/app_route_names.dart';
@@ -57,7 +58,7 @@ class AppRouteConfig {
       GoRoute(
         name: AppRouteNames.selectPracticeDoctor,
         path: '/selectPracticeDoctor', // Selecta Practice and Doctor Pages
-        builder: ((context, state) => Container()),
+        builder: ((context, state) => const SelectPractice()),
       ),
       GoRoute(
         name: AppRouteNames.onboarding,
